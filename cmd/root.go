91e131f0608f75099eee0edb8b436f1838289c76
Ezzaldeen Mousa <ezzaldeeenmousa@gmail.com>
2023-03-08 15:41:31 +0200

    prepare cli structure

---
+++ cmd/root.go
@@ -0,0 +1,28 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+var version = "0.0.1"
+
+var rootCmd = &cobra.Command{
+	Use:     "worder",
+	Version: version,
+	Short:   "worder - simple CLI to generate and count words",
+	Long: `Worder CLI is a word generator, and counter.
+Uses a sample from Lorem Ipsum to generate files based on different count, and size
+and provide the capability to count the number of words in these generated files.`,
+	Run: func(cmd *cobra.Command, args []string) {
+
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "Whoops. There was an error while executing your CLI '%s'\n", err)
+		os.Exit(1)
+	}
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "worder/cmd"
+
+func main() {
+	cmd.Execute()
+}
