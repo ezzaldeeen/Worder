e16edd1494aff1f8e3586890cb2866caf72922ec
Ezzaldeen Mousa <ezzaldeeenmousa@gmail.com>
2023-04-28 17:29:54 +0200

    add files dispatcher and word counter

---
+++ counter/word_counter.go
@@ -0,0 +1,53 @@
+package counter
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path"
+	"sync"
+)
+
+type FilesDispatcher struct {
+	directoryPath string
+	filePaths     chan<- string
+}
+
+func FileDispatcher(dirPath string, ch chan<- string) *FilesDispatcher {
+	return &FilesDispatcher{
+		directoryPath: dirPath,
+		filePaths:     ch,
+	}
+}
+
+func (fd FilesDispatcher) Dispatch() error {
+	defer close(fd.filePaths)
+	entries, err := os.ReadDir(fd.directoryPath)
+	if err != nil {
+		return err
+	}
+	for _, entry := range entries {
+		filePath := path.Join(fd.directoryPath, entry.Name())
+		fd.filePaths <- filePath
+	}
+	return nil
+}
+
+type WordCounter struct {
+	filePaths <-chan string
+}
+
+func NewWordCounter(ch <-chan string) *WordCounter {
+	return &WordCounter{
+		filePaths: ch,
+	}
+}
+
+func (wc WordCounter) Run(ctx context.Context, wg *sync.WaitGroup) {
+	select {
+	case <-ctx.Done():
+		fmt.Println("")
+	default:
+		fmt.Println("work")
+	}
+}
--- main.go
+++ main.go
@@ -1,5 +1,35 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"path"
+	"time"
+)
+
 func main() {
+	ch := make(chan string)
+
+	entries, err := os.ReadDir("data")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for i := 0; i < 2; i++ {
+		go func() {
+			for _, entry := range entries {
+				filePath := path.Join("data", entry.Name())
+				ch <- filePath
+			}
+		}()
+	}
+
+	go func() {
+		for filePath := range ch {
+			fmt.Println(filePath)
+		}
+	}()
+
+	time.Sleep(5 * time.Second)
 
 }
