259d522eaafefedf8327a7232375b6eaa1b14082
Ezzaldeen Mousa <ezzaldeeenmousa@gmail.com>
2023-04-28 14:44:21 +0200

    add worker pool

---
+++ workerpool/worker_pool.go
@@ -0,0 +1,44 @@
+package workerpool
+
+import (
+	"context"
+	"sync"
+)
+
+// Runnable for the executable tasks by the worker pool
+type Runnable interface {
+	Run(ctx context.Context, wg *sync.WaitGroup)
+}
+
+// WorkerPool the controller of the execution behavior
+type WorkerPool struct {
+	numOfWorkers int
+	runnable     Runnable
+	ctx          context.Context
+	cancelFn     context.CancelFunc
+}
+
+func NewWorkerPool(numOfWorkers int, runnable Runnable) *WorkerPool {
+	ctx, cancel := context.WithCancel(context.Background())
+	return &WorkerPool{
+		numOfWorkers: numOfWorkers,
+		runnable:     runnable,
+		ctx:          ctx,
+		cancelFn:     cancel,
+	}
+}
+
+// Start starting the execution based on the number of workers
+func (wp *WorkerPool) Start(wg *sync.WaitGroup) {
+	for i := 0; i < wp.numOfWorkers; i++ {
+		go func(wg *sync.WaitGroup) {
+			wp.runnable.Run(wp.ctx, wg)
+		}(wg)
+	}
+	wg.Wait()
+}
+
+// Stop canceling the execution by cancel signal
+func (wp *WorkerPool) Stop() {
+	wp.cancelFn()
+}
