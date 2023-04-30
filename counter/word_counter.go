2b583149599858d6f183814fd05679b9d14dde2d
Ezzaldeen Mousa <ezzaldeeenmousa@gmail.com>
2023-04-30 11:31:02 +0300

    word counter logic

--- counter/word_counter.go
+++ counter/word_counter.go
@@ -1,38 +1,45 @@
 package counter
 
 import (
+	"bufio"
 	"context"
+	"go.uber.org/zap"
 	"os"
 	"path"
 	"sync"
 )
 
+// FilesDispatcher sending the file paths through paths channel
 type FilesDispatcher struct {
-	directoryPath string
-	filePaths     chan<- string
+	source string
+	paths  chan<- string
 }
 
-func FileDispatcher(dirPath string, ch chan<- string) *FilesDispatcher {
+// NewFileDispatcher factory function
+func NewFileDispatcher(src string, ch chan<- string) *FilesDispatcher {
 	return &FilesDispatcher{
-		directoryPath: dirPath,
-		filePaths:     ch,
+		source: src,
+		paths:  ch,
 	}
 }
 
+// Dispatch operation for listing the files in the given directory (source)
+// and sending the file paths in that dir through a paths channel
 func (fd FilesDispatcher) Dispatch() error {
-	defer close(fd.filePaths)
-	entries, err := os.ReadDir(fd.directoryPath)
+	defer close(fd.paths)
+	entries, err := os.ReadDir(fd.source)
 	if err != nil {
 		return err
 	}
 	for _, entry := range entries {
-		filePath := path.Join(fd.directoryPath, entry.Name())
-		fd.filePaths <- filePath
+		filePath := path.Join(fd.source, entry.Name())
+		fd.paths <- filePath
 	}
 	return nil
 }
 
 type WordCounter struct {
+	logger    zap.Logger
 	filePaths <-chan string
 }
 
@@ -45,6 +52,27 @@ func NewWordCounter(ch <-chan string) *WordCounter {
 func (wc WordCounter) Run(ctx context.Context, wg *sync.WaitGroup) {
 	select {
 	case <-ctx.Done():
+		wc.logger.Error("context cancelled")
 	default:
+		filePath := <-wc.filePaths
+		file, err := os.Open(filePath)
+		defer file.Close()
+		if err != nil {
+			wc.logger.Error("unable to open the file:", zap.Field{
+				Key:    "path",
+				String: filePath,
+			})
+		}
+
+		reader := bufio.NewReader(file)
+		for {
+			b, err := reader.ReadByte()
+			if err != nil {
+				return
+			}
+			if b == ' ' {
+				// todo: increase counter
+			}
+		}
 	}
 }
